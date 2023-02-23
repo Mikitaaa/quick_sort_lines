d80ed77a962e93606525a1e58a8722361f5e9456
Mek <nik.podterob@gmail.com>
2023-02-23 23:38:17 +0300

    Initial Commit

---
+++ quick_sort_lines/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  quick_sort_lines
+//
+//  Created by Никита Подтероб on 23.02.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
